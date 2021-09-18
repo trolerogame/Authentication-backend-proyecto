2ecc3a2b77d324b83571e8d3da6be86cc6882a9e
Daniel Campos <danielgcampos.04@gmail.com>
2021-09-17 21:21:01 -0300

    prueba 3

--- dist/index.js
+++ dist/index.js
@@ -1,50 +1,31 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-const express_1 = __importStar(require("express"));
-const express_graphql_1 = require("express-graphql");
-const schema_1 = require("@graphql-tools/schema");
-const path_1 = require("path");
-const fs_1 = require("fs");
-const cors_1 = __importDefault(require("cors"));
-const resolvers_1 = __importDefault(require("./controllers/resolvers"));
-require("./db/connect");
-// initials
+const express_1 = __importDefault(require("express"));
+// import {graphqlHTTP} from 'express-graphql'
+// import {makeExecutableSchema} from '@graphql-tools/schema'
+// import { join } from 'path'
+// import {readFileSync} from 'fs'
+// import cors from 'cors'
+// import resolvers from './controllers/resolvers'
+// import './db/connect'
+// // initials
 const app = (0, express_1.default)();
-const typeDefs = (0, fs_1.readFileSync)((0, path_1.join)(__dirname, 'schemas.graphql'), 'utf-8');
-const schema = (0, schema_1.makeExecutableSchema)({ typeDefs, resolvers: resolvers_1.default });
+// const typeDefs = readFileSync(join(__dirname,'schemas.graphql'),'utf-8')
+// const schema = makeExecutableSchema({typeDefs,resolvers})
 // configs
-app.use((0, cors_1.default)());
-app.use((0, express_1.json)());
-app.use((0, express_1.urlencoded)({ extended: true }));
-app.use('/graphql', (0, express_graphql_1.graphqlHTTP)({
-    schema,
-    rootValue: resolvers_1.default,
-    graphiql: false
-}));
+// app.use(cors({optionsSuccessStatus: 200}))
+// app.use(json())
+// app.use(urlencoded({ extended: true }))
+// app.use('/graphql',graphqlHTTP({
+//     schema,
+//     rootValue:resolvers,
+//     graphiql:false
+// }))
 app.get('/', (req, res) => {
     res.send('hola mundo');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 import {config} from 'dotenv'
 config()
-import express,{json,urlencoded} from 'express'
-import {graphqlHTTP} from 'express-graphql'
-import {makeExecutableSchema} from '@graphql-tools/schema'
-import { join } from 'path'
-import {readFileSync} from 'fs'
-import cors from 'cors'
-import resolvers from './controllers/resolvers'
-import './db/connect'
+import express from 'express'
+// import {graphqlHTTP} from 'express-graphql'
+// import {makeExecutableSchema} from '@graphql-tools/schema'
+// import { join } from 'path'
+// import {readFileSync} from 'fs'
+// import cors from 'cors'
+// import resolvers from './controllers/resolvers'
+// import './db/connect'
 
-// initials
+// // initials
 const app = express()
-const typeDefs = readFileSync(join(__dirname,'schemas.graphql'),'utf-8')
-const schema = makeExecutableSchema({typeDefs,resolvers})
+// const typeDefs = readFileSync(join(__dirname,'schemas.graphql'),'utf-8')
+// const schema = makeExecutableSchema({typeDefs,resolvers})
 
 // configs
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use('/graphql',graphqlHTTP({
-    schema,
-    rootValue:resolvers,
-    graphiql:false
-}))
+// app.use(cors({optionsSuccessStatus: 200}))
+// app.use(json())
+// app.use(urlencoded({ extended: true }))
+// app.use('/graphql',graphqlHTTP({
+//     schema,
+//     rootValue:resolvers,
+//     graphiql:false
+// }))
 
 app.get('/',(req,res) => {
     res.send('hola mundo')

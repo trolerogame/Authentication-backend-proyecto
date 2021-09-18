dc24a32a3a48ecc6ae8df0a1d6437ae90be1c3ab
Daniel Campos <danielgcampos.04@gmail.com>
2021-09-17 22:52:01 -0300

    apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
 import {config} from 'dotenv'
 config()
 import express from 'express'
-import {ApolloServer} from 'apollo-server-express'
+import {ApolloServer,gql} from 'apollo-server-express'
 import cors from 'cors'
-import {typeDefs} from './types'
+// import {typeDefs} from './types'
 import resolvers from './controllers/resolvers'
 import './db/connect'
+const typeDefs = gql`
+type User{
+    _id:ID,
+    username:String
+    email:String,
+    password:String,
+    bio:String,
+    phone:String,
+    passwordLength:String
+}
+type Query{
+    "devuelve un usuario"
+    getUser(id:ID!):User
+}
+input createUserType {
+    email:String!,
+    password:String!,
+}
+input editUserType {
+    username:String,
+    email:String,
+    password:String,
+    bio:String,
+    phone:String
+}
 
+type Mutation {
+    "crea un usuario"
+    createUser(input:createUserType):User
+    "edita el usuario"
+    editUser(id:ID!,input:editUserType):User
+    "elimina un usuario"
+    deleteUser(id:ID!):User
+    "logeamos un usuario"
+    loginUser(email:String,password:String):User
+}
+`
 // initials
 const app = express()
 // configs

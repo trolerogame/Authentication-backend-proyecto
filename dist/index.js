ac7d71bdbdf389ae4cbf2364adbb04c32a13748a
Daniel Campos <danielgcampos.04@gmail.com>
2021-09-17 23:38:34 -0300

    prueba

--- dist/index.js
+++ dist/index.js
@@ -8,53 +8,52 @@ const dotenv_1 = require("dotenv");
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const cors_1 = __importDefault(require("cors"));
-// import {typeDefs} from './types'
 const resolvers_1 = __importDefault(require("./controllers/resolvers"));
+const TypeDefs_1 = require("./TypeDefs");
 require("./db/connect");
-const typeDefs = (0, apollo_server_express_1.gql) `
-type User{
-    _id:ID,
-    username:String
-    email:String,
-    password:String,
-    bio:String,
-    phone:String,
-    passwordLength:String
-}
-type Query{
-    "devuelve un usuario"
-    getUser(id:ID!):User
-}
-input createUserType {
-    email:String!,
-    password:String!,
-}
-input editUserType {
-    username:String,
-    email:String,
-    password:String,
-    bio:String,
-    phone:String
-}
-
-type Mutation {
-    "crea un usuario"
-    createUser(input:createUserType):User
-    "edita el usuario"
-    editUser(id:ID!,input:editUserType):User
-    "elimina un usuario"
-    deleteUser(id:ID!):User
-    "logeamos un usuario"
-    loginUser(email:String,password:String):User
-}
-`;
+// const typeDefs = gql`
+// type User{
+//     _id:ID,
+//     username:String
+//     email:String,
+//     password:String,
+//     bio:String,
+//     phone:String,
+//     passwordLength:String
+// }
+// type Query{
+//     "devuelve un usuario"
+//     getUser(id:ID!):User
+// }
+// input createUserType {
+//     email:String!,
+//     password:String!,
+// }
+// input editUserType {
+//     username:String,
+//     email:String,
+//     password:String,
+//     bio:String,
+//     phone:String
+// }
+// type Mutation {
+//     "crea un usuario"
+//     createUser(input:createUserType):User
+//     "edita el usuario"
+//     editUser(id:ID!,input:editUserType):User
+//     "elimina un usuario"
+//     deleteUser(id:ID!):User
+//     "logeamos un usuario"
+//     loginUser(email:String,password:String):User
+// }
+// `
 // initials
 const app = (0, express_1.default)();
 // configs
 app.use(express_1.default.urlencoded({ extended: true }));
 const server = new apollo_server_express_1.ApolloServer({
     resolvers: resolvers_1.default,
-    typeDefs,
+    typeDefs: TypeDefs_1.typeDefs,
 });
 server.start().then(() => {
     server.applyMiddleware({ app, path: '/graphql' });

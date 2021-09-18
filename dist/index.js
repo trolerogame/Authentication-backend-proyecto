b8207b9bd7c075cb164c3a63de213d10cb95a803
Daniel Campos <danielgcampos.04@gmail.com>
2021-09-17 22:47:19 -0300

    apollo-server-express

--- dist/index.js
+++ dist/index.js
@@ -6,59 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const express_1 = __importDefault(require("express"));
-const schema_1 = require("@graphql-tools/schema");
+const apollo_server_express_1 = require("apollo-server-express");
 const cors_1 = __importDefault(require("cors"));
+const types_1 = require("./types");
 const resolvers_1 = __importDefault(require("./controllers/resolvers"));
 require("./db/connect");
 // initials
 const app = (0, express_1.default)();
-const typeDefs = `
-    type User{
-        _id:ID,
-        username:String
-        email:String,
-        password:String,
-        bio:String,
-        phone:String,
-        passwordLength:String
-    }
-    type Query{
-        "devuelve un usuario"
-        getUser(id:ID!):User
-    }
-    input createUserType {
-        email:String!,
-        password:String!,
-    }
-    input editUserType {
-        username:String,
-        email:String,
-        password:String,
-        bio:String,
-        phone:String
-    }
-
-    type Mutation {
-        "crea un usuario"
-        createUser(input:createUserType):User
-        "edita el usuario"
-        editUser(id:ID!,input:editUserType):User
-        "elimina un usuario"
-        deleteUser(id:ID!):User
-        "logeamos un usuario"
-        loginUser(email:String,password:String):User
-    }
-`;
-const schema = (0, schema_1.makeExecutableSchema)({ typeDefs, resolvers: resolvers_1.default });
 // configs
+app.use(express_1.default.urlencoded({ extended: true }));
+const server = new apollo_server_express_1.ApolloServer({
+    resolvers: resolvers_1.default,
+    typeDefs: types_1.typeDefs,
+});
+server.start().then(() => {
+    server.applyMiddleware({ app, path: '/graphql' });
+});
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-// app.use('/graphql',graphqlHTTP({
-//     schema,
-//     rootValue:resolvers,
-//     graphiql:false
-// }))
 app.get('/', (req, res) => {
     res.send('hola mundo');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,25 @@
 import {config} from 'dotenv'
 config()
 import express from 'express'
-import {graphqlHTTP} from 'express-graphql'
-import {makeExecutableSchema} from '@graphql-tools/schema'
+import {ApolloServer} from 'apollo-server-express'
 import cors from 'cors'
+import {typeDefs} from './types'
 import resolvers from './controllers/resolvers'
 import './db/connect'
 
 // initials
 const app = express()
-const typeDefs = `
-    type User{
-        _id:ID,
-        username:String
-        email:String,
-        password:String,
-        bio:String,
-        phone:String,
-        passwordLength:String
-    }
-    type Query{
-        "devuelve un usuario"
-        getUser(id:ID!):User
-    }
-    input createUserType {
-        email:String!,
-        password:String!,
-    }
-    input editUserType {
-        username:String,
-        email:String,
-        password:String,
-        bio:String,
-        phone:String
-    }
-
-    type Mutation {
-        "crea un usuario"
-        createUser(input:createUserType):User
-        "edita el usuario"
-        editUser(id:ID!,input:editUserType):User
-        "elimina un usuario"
-        deleteUser(id:ID!):User
-        "logeamos un usuario"
-        loginUser(email:String,password:String):User
-    }
-`
-const schema = makeExecutableSchema({typeDefs,resolvers})
-
 // configs
+app.use(express.urlencoded({ extended: true }))
+const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+})
+server.start().then(() => {
+    server.applyMiddleware({ app, path: '/graphql' })
+})
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-// app.use('/graphql',graphqlHTTP({
-//     schema,
-//     rootValue:resolvers,
-//     graphiql:false
-// }))
 
 app.get('/',(req,res) => {
     res.send('hola mundo')

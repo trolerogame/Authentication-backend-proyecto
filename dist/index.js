d6433865833630d2f8e983dbcdf1b5cc67668419
Daniel Campos <danielgcampos.04@gmail.com>
2021-09-17 23:49:23 -0300

    typeDefs en el index

--- dist/index.js
+++ dist/index.js
@@ -9,51 +9,51 @@ const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const cors_1 = __importDefault(require("cors"));
 const resolvers_1 = __importDefault(require("./controllers/resolvers"));
-const TypeDefs_1 = require("./model/TypeDefs");
 require("./db/connect");
-// const typeDefs = gql`
-// type User{
-//     _id:ID,
-//     username:String
-//     email:String,
-//     password:String,
-//     bio:String,
-//     phone:String,
-//     passwordLength:String
-// }
-// type Query{
-//     "devuelve un usuario"
-//     getUser(id:ID!):User
-// }
-// input createUserType {
-//     email:String!,
-//     password:String!,
-// }
-// input editUserType {
-//     username:String,
-//     email:String,
-//     password:String,
-//     bio:String,
-//     phone:String
-// }
-// type Mutation {
-//     "crea un usuario"
-//     createUser(input:createUserType):User
-//     "edita el usuario"
-//     editUser(id:ID!,input:editUserType):User
-//     "elimina un usuario"
-//     deleteUser(id:ID!):User
-//     "logeamos un usuario"
-//     loginUser(email:String,password:String):User
-// }
-// `
+const typeDefs = (0, apollo_server_express_1.gql) `
+type User{
+    _id:ID,
+    username:String
+    email:String,
+    password:String,
+    bio:String,
+    phone:String,
+    passwordLength:String
+}
+type Query{
+    "devuelve un usuario"
+    getUser(id:ID!):User
+}
+input createUserType {
+    email:String!,
+    password:String!,
+}
+input editUserType {
+    username:String,
+    email:String,
+    password:String,
+    bio:String,
+    phone:String
+}
+
+type Mutation {
+    "crea un usuario"
+    createUser(input:createUserType):User
+    "edita el usuario"
+    editUser(id:ID!,input:editUserType):User
+    "elimina un usuario"
+    deleteUser(id:ID!):User
+    "logeamos un usuario"
+    loginUser(email:String,password:String):User
+}
+`;
 // initials
 const app = (0, express_1.default)();
 // configs
 app.use(express_1.default.urlencoded({ extended: true }));
 const server = new apollo_server_express_1.ApolloServer({
     resolvers: resolvers_1.default,
-    typeDefs: TypeDefs_1.typeDefs,
+    typeDefs,
 });
 server.start().then(() => {
     server.applyMiddleware({ app, path: '/graphql' });
--- src/index.ts
+++ src/index.ts
@@ -3,46 +3,45 @@ config()
 import express from 'express'
 import {ApolloServer,gql} from 'apollo-server-express'
 import cors from 'cors'
-import resolvers from './controllers/resolvers'
-import {typeDefs} from './model/TypeDefs'
+import resolvers from './controllers/resolvers' 
 import './db/connect'
-// const typeDefs = gql`
-// type User{
-//     _id:ID,
-//     username:String
-//     email:String,
-//     password:String,
-//     bio:String,
-//     phone:String,
-//     passwordLength:String
-// }
-// type Query{
-//     "devuelve un usuario"
-//     getUser(id:ID!):User
-// }
-// input createUserType {
-//     email:String!,
-//     password:String!,
-// }
-// input editUserType {
-//     username:String,
-//     email:String,
-//     password:String,
-//     bio:String,
-//     phone:String
-// }
+const typeDefs = gql`
+type User{
+    _id:ID,
+    username:String
+    email:String,
+    password:String,
+    bio:String,
+    phone:String,
+    passwordLength:String
+}
+type Query{
+    "devuelve un usuario"
+    getUser(id:ID!):User
+}
+input createUserType {
+    email:String!,
+    password:String!,
+}
+input editUserType {
+    username:String,
+    email:String,
+    password:String,
+    bio:String,
+    phone:String
+}
 
-// type Mutation {
-//     "crea un usuario"
-//     createUser(input:createUserType):User
-//     "edita el usuario"
-//     editUser(id:ID!,input:editUserType):User
-//     "elimina un usuario"
-//     deleteUser(id:ID!):User
-//     "logeamos un usuario"
-//     loginUser(email:String,password:String):User
-// }
-// `
+type Mutation {
+    "crea un usuario"
+    createUser(input:createUserType):User
+    "edita el usuario"
+    editUser(id:ID!,input:editUserType):User
+    "elimina un usuario"
+    deleteUser(id:ID!):User
+    "logeamos un usuario"
+    loginUser(email:String,password:String):User
+}
+`
 // initials
 const app = express()
 // configs
